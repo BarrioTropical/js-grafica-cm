9a9e7e1f3661cc8b7c9f5d8eafaa99e82350284a
“BarrioTropical” <barriotropical@gmail.com>
2022-10-17 18:16:19 +0200

    creazione griglia
    Signed-off-by: “BarrioTropical” <“barriotropical@gmail.com”>

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 
+/*
+<div class="grid">
+    <div class="square">
+    <span>1</span>
+    </div>
+</div>
+*/
+
 const playButton = document.getElementById('play');
 
 function play(){
-    console.log('il bottone funziona')
+    console.log('il bottone funziona');
+    const numCell = 100;
 
+    function drawCell(numCell){
+        const cell = document.createElement('div');
+        cell.className = 'square';
+        cell.innerHTML = `
+            <span>${numCell}</span>
+        `;
+        return cell;
+    }
+
+    
+    function drawGrid(){
+        const fieldGame = document.getElementById('field-game');
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        for(let i = 1; i <= 100; i++){
+            const cell = drawCell(i);
+            grid.appendChild(cell);
+        }
+        fieldGame.appendChild(grid);
+    }
+
+    drawGrid();
 }
 
 playButton.addEventListener('click', play);
