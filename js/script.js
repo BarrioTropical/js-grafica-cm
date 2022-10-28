6d233f7f7cef70a9b2594e8d43cc225143089e23
BarrioTropical <98409091+BarrioTropical@users.noreply.github.com>
2022-10-28 17:35:34 +0200

    modifiche
    Signed-off-by: BarrioTropical <98409091+BarrioTropical@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -11,8 +11,22 @@ const playButton = document.getElementById('play');
 
 function play(){
     console.log('il bottone funziona');
-    const numCell = 100;
+    const numCell
+    const levelHtml = document.getElementById('livello');
+    const level = levelHtml.value;
+    switch(level){
+        case '1':
+            numCell = 100;
+            break;
+        case '2':
+            numCell = 81;
+            break;
+        case '3':
+            numCell = 49;
+            break;
+    }
 
+    //creo la cella
     function drawCell(numCell){
         const cell = document.createElement('div');
         cell.className = 'square';
@@ -25,18 +39,19 @@ function play(){
         return cell;
     }
 
-    
+    //creo la griglia
     function drawGrid(){
         const fieldGame = document.getElementById('field-game');
         const grid = document.createElement('div');
         grid.className = 'grid';
-        for(let i = 1; i <= 100; i++){
+        for(let i = 1; i <= numCell; i++){
             const cell = drawCell(i);
             grid.appendChild(cell);
         }
         fieldGame.appendChild(grid);
     }
 
+    //avvia griglia e quindi cella
     drawGrid();
 }
 
